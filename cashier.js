fb23ac1335893fce53c41d52c1a88db39a0089fa
Ariq Fikri Tamamy <ariqrick@gmail.com>
2026-01-27 04:43:06 +0700

    Modified Cashier Page

--- cashier.js
+++ cashier.js
@@ -577,6 +577,8 @@ payButton.addEventListener("click", () => {
 
     if (Object.keys(activeOrders).length === 0) return;
 
+    const tableText = document.querySelector(".selected-text")?.innerText || "-";
+
     const nominalInput = document.getElementById("nominalInput");
     const paid = parseInt(nominalInput.value.replace(/\D/g, "") || 0);
 
@@ -593,6 +595,7 @@ payButton.addEventListener("click", () => {
 
     showSuccessModal({
         activeOrders,
+        tableText,
         subTotal,
         tax,
         total,
@@ -608,6 +611,8 @@ function showSuccessModal({ activeOrders, subTotal, tax, total, paid }) {
         document.getElementById("customer-name").value || "-";
     document.getElementById("receiptType").innerText =
         currentOrderType === "dinein" ? "Dine In" : "Take Away";
+    document.getElementById("tableNumber").innerText = 
+        document.querySelector(".selected-text")?.innerText || "-";
 
     const itemsContainer = document.getElementById("receiptItems");
     itemsContainer.innerHTML = "";
@@ -618,8 +623,11 @@ function showSuccessModal({ activeOrders, subTotal, tax, total, paid }) {
 
         row.innerHTML = `
             <div class="receipt-item-row">
-                <span>${item.name} x${item.qty}</span>
-                <span>Rp ${format(item.price * item.qty)}</span>
+                <div class="receipt-name-price-item">
+                    <span>${item.name}</span>
+                    <span id="priceQty">${format(item.price * item.qty)}</span>
+                </div>
+                <span id="qtyXPrice">${item.qty} x Rp ${item.price} </span>
             </div>
             ${item.note ? `<small class="receipt-note">Catatan: ${item.note}</small>` : ""}
         `;
@@ -633,7 +641,6 @@ function showSuccessModal({ activeOrders, subTotal, tax, total, paid }) {
         `Rp ${format(tax)}`;
     document.getElementById("receiptTotal").innerText =
         `Rp ${format(total)}`;
-
     document.getElementById("receiptPaid").innerText =
         `Rp ${format(paid)}`;
     document.getElementById("receiptChange").innerText =
@@ -653,3 +660,354 @@ window.addEventListener("click", e => {
         resetOrderAfterArchive();
     }
 });
+
+// Object Menu
+const menus = [
+  // ================= FOODS =================
+  {
+    id: 1,
+    category: "food",
+    name: "Nasi Goreng Kampung",
+    image: "https://images.unsplash.com/photo-1668839746796-c5bca3982957",
+    description: "Nasi goreng tradisional dengan bawang, cabai, telur, dan ayam.",
+    price: 25000
+  },
+  {
+    id: 2,
+    category: "food",
+    name: "Rendang Daging Sapi",
+    image: "https://images.unsplash.com/photo-1601050690597-df0568f70950",
+    description: "Daging sapi dimasak dengan santan dan rempah khas Minang.",
+    price: 45000
+  },
+  {
+    id: 3,
+    category: "food",
+    name: "Sate Ayam Madura",
+    image: "https://images.unsplash.com/photo-1627308595229-7830a5c91f9f",
+    description: "Sate ayam dengan saus kacang khas Madura.",
+    price: 30000
+  },
+  {
+    id: 4,
+    category: "food",
+    name: "Gado-Gado",
+    image: "https://images.unsplash.com/photo-1707269561481-a4a0370a980a",
+    description: "Sayuran rebus dengan saus kacang dan kerupuk.",
+    price: 20000
+  },
+  {
+    id: 5,
+    category: "food",
+    name: "Soto Ayam",
+    image: "https://www.shutterstock.com/image-photo/soto-ayam-typical-indonesian-food-260nw-2517244091.jpg",
+    description: "Soto ayam dengan kuah kuning dan koya.",
+    price: 25000
+  },
+  {
+    id: 6,
+    category: "food",
+    name: "Bakso Sapi",
+    image: "https://img.freepik.com/foto-gratis/sudut-pandang-tinggi-susunan-mangkuk-bakso-lezat_23-2148967915.jpg?semt=ais_hybrid&w=740&q=80",
+    description: "Bakso sapi dengan kuah kaldu dan mie.",
+    price: 22000
+  },
+  {
+    id: 7,
+    category: "food",
+    name: "Ayam Geprek",
+    image: "https://www.shutterstock.com/image-photo/ayam-geprek-indonesian-food-crispy-600nw-1949306203.jpg",
+    description: "Ayam goreng tepung dengan sambal pedas.",
+    price: 23000
+  },
+  {
+    id: 8,
+    category: "food",
+    name: "Nasi Uduk",
+    image: "https://www.shutterstock.com/image-photo/nasi-uduk-dish-made-diaroned-260nw-2510230781.jpg",
+    description: "Nasi gurih santan dengan lauk khas Betawi.",
+    price: 20000
+  },
+  {
+    id: 9,
+    category: "food",
+    name: "Pempek Palembang",
+    image: "https://www.shutterstock.com/image-photo/pempek-mpekmpek-indonesian-slang-empekempek-260nw-2586079891.jpg",
+    description: "Olahan ikan dengan kuah cuko asam pedas.",
+    price: 28000
+  },
+  {
+    id: 10,
+    category: "food",
+    name: "Rawon",
+    image: "https://www.shutterstock.com/image-photo/rawon-indonesian-black-beef-soup-260nw-2286219153.jpg",
+    description: "Sup daging sapi dengan kuah hitam khas Jawa Timur.",
+    price: 35000
+  },
+
+  // ================= BEVERAGES =================
+  {
+    id: 11,
+    category: "beverages",
+    name: "Es Teh Manis",
+    image: "https://png.pngtree.com/png-vector/20240519/ourmid/pngtree-jumbo-iced-tea-transparent-background-png-image_12495350.png",
+    description: "Teh manis dingin segar.",
+    price: 5000
+  },
+  {
+    id: 12,
+    category: "beverages",
+    name: "Es Jeruk",
+    image: "https://img.freepik.com/foto-gratis/segelas-jus-jeruk-diletakkan-di-atas-kayu_1150-9661.jpg?semt=ais_hybrid&w=740&q=80",
+    description: "Perasan jeruk segar dengan es batu.",
+    price: 7000
+  },
+  {
+    id: 13,
+    category: "beverages",
+    name: "Es Cendol",
+    image: "https://www.shutterstock.com/image-photo/es-campur-cendol-cincau-600nw-2585426799.jpg",
+    description: "Cendol dengan santan dan gula aren.",
+    price: 12000
+  },
+  {
+    id: 14,
+    category: "beverages",
+    name: "Es Campur",
+    image: "https://st5.depositphotos.com/76634246/64711/i/450/depositphotos_647110546-stock-photo-fruit-soup-soup-prepared-using.jpg",
+    description: "Campuran buah, cincau, dan sirup.",
+    price: 15000
+  },
+  {
+    id: 15,
+    category: "beverages",
+    name: "Es Kelapa Muda",
+    image: "https://st.depositphotos.com/52939322/55514/i/450/depositphotos_555142042-stock-photo-kelapa-jeruk-typical-indonesian-drink.jpg",
+    description: "Air kelapa muda segar.",
+    price: 10000
+  },
+  {
+    id: 16,
+    category: "beverages",
+    name: "Bajigur",
+    image: "https://img-global.cpcdn.com/recipes/b680cdaf57abb4b9/228x268cq80/photo.jpg",
+    description: "Minuman hangat santan dan gula aren.",
+    price: 12000
+  },
+  {
+    id: 17,
+    category: "beverages",
+    name: "Bandrek",
+    image: "https://img-global.cpcdn.com/recipes/b8c7139f9d7ef649/200x200cq80/bandrek-foto-resep-utama.jpg",
+    description: "Minuman jahe khas Sunda.",
+    price: 12000
+  },
+  {
+    id: 18,
+    category: "beverages",
+    name: "Wedang Jahe",
+    image: "https://www.shutterstock.com/image-photo/wedang-jahe-one-indonesian-traditional-260nw-1707637867.jpg",
+    description: "Minuman jahe hangat tradisional.",
+    price: 10000
+  },
+  {
+    id: 19,
+    category: "beverages",
+    name: "Es Dawet Ayu",
+    image: "https://www.shutterstock.com/image-photo/dawet-ayu-traditional-drink-made-600nw-2340438975.jpg",
+    description: "Dawet khas Banjarnegara.",
+    price: 12000
+  },
+  {
+    id: 20,
+    category: "beverages",
+    name: "Kopi Tubruk",
+    image: "https://images.unsplash.com/photo-1509042239860-f550ce710b93",
+    description: "Kopi hitam tradisional Indonesia.",
+    price: 8000
+  },
+
+  // ================= DESSERT =================
+  {
+    id: 21,
+    category: "dessert",
+    name: "Klepon",
+    image: "https://www.shutterstock.com/image-photo/klepon-indonesian-popular-traditional-snack-260nw-2015476712.jpg",
+    description: "Kue ketan isi gula merah.",
+    price: 10000
+  },
+  {
+    id: 22,
+    category: "dessert",
+    name: "Onde-Onde",
+    image: "https://www.shutterstock.com/image-photo/onde-cake-on-table-260nw-2416199051.jpg",
+    description: "Kue wijen isi kacang hijau.",
+    price: 10000
+  },
+  {
+    id: 23,
+    category: "dessert",
+    name: "Dadar Gulung",
+    image: "https://cdn.grid.id/crop/0x0:0x0/filters:format(webp):quality(100)/photo/2020/06/06/135383365.jpg",
+    description: "Pancake pandan isi kelapa manis.",
+    price: 12000
+  },
+  {
+    id: 24,
+    category: "dessert",
+    name: "Getuk",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4-QU5bY4URw81BAt3USxfrjMFXSeSSUmjAA&s",
+    description: "Singkong tumbuk dengan gula.",
+    price: 9000
+  },
+  {
+    id: 25,
+    category: "dessert",
+    name: "Lapis Legit",
+    image: "https://png.pngtree.com/png-vector/20250429/ourmid/pngtree-lapis-legit-cake-png-image_16069109.png",
+    description: "Kue lapis khas Indonesia.",
+    price: 15000
+  },
+  {
+    id: 26,
+    category: "dessert",
+    name: "Serabi",
+    image: "https://www.shutterstock.com/image-photo/this-traditional-food-indonesia-called-260nw-2483781697.jpg",
+    description: "Pancake tradisional dengan santan.",
+    price: 10000
+  },
+  {
+    id: 27,
+    category: "dessert",
+    name: "Kue Lumpur",
+    image: "https://hypeabis.id/assets/content/202309211340191695278419.jpg",
+    description: "Kue lembut berbahan kentang.",
+    price: 12000
+  },
+  {
+    id: 28,
+    category: "dessert",
+    name: "Bubur Sumsum",
+    image: "https://www.shutterstock.com/image-photo/rice-flour-coconut-milk-porridge-600nw-2252590039.jpg",
+    description: "Bubur lembut dengan saus gula merah.",
+    price: 10000
+  },
+  {
+    id: 29,
+    category: "dessert",
+    name: "Es Pisang Ijo",
+    image: "https://www.shutterstock.com/image-photo/rice-flour-coconut-milk-porridge-600nw-2252590039.jpg",
+    description: "Pisang dibalut adonan hijau dengan sirup.",
+    price: 15000
+  },
+  {
+    id: 30,
+    category: "dessert",
+    name: "Kue Putu",
+    image: "https://www.shutterstock.com/image-photo/putu-cake-kue-ayu-cakes-260nw-2233786323.jpg",
+    description: "Kue bambu isi gula merah.",
+    price: 10000
+  }
+];
+
+      const menuListElement = document.getElementById("menuList");
+
+      function renderMenuList(data) {
+          menuListElement.innerHTML = "";
+
+          data.forEach(menu => {
+              const card = document.createElement("div");
+              card.className = "menu-list-card";
+              card.dataset.category = menu.category;
+              card.dataset.id = menu.id;
+              card.dataset.name = menu.name;
+              card.dataset.price = menu.price;
+              card.dataset.image = menu.image;
+
+              card.innerHTML = `
+                  <div class="menu-content">
+                      <div class="menu-image">
+                          <img src="${menu.image}" alt="${menu.name}">
+                          <div class="category-badge">
+                              <p>${capitalize(menu.category)}</p>
+                          </div>
+                      </div>
+
+                      <div class="menu-name">
+                          <h1>${menu.name}</h1>
+                      </div>
+
+                      <div class="menu-description">
+                          <p>${menu.description}</p>
+                      </div>
+
+                      <div class="menu-price">
+                          <p><span>Rp. ${format(menu.price)}</span>/portion</p>
+                      </div>
+
+                      <div class="detail-menu-icon">
+                          <a href="#">
+                              <img src="assets/order-menu-icon.png" alt="Order Menu Icon">
+                          </a>
+                      </div>
+                  </div>
+              `;
+
+              menuListElement.appendChild(card);
+          });
+
+          updateTotalMenu(data.length);
+      }
+
+      function capitalize(text) {
+          return text.charAt(0).toUpperCase() + text.slice(1);
+      }
+
+      document.addEventListener("DOMContentLoaded", () => {
+        renderMenuList(menus);
+      });
+
+      const categoryMenuButtons = document.querySelectorAll(".category-menu > div");
+
+      categoryMenuButtons.forEach(button => {
+          button.addEventListener("click", () => {
+              const filter = button.dataset.filter;
+
+              categoryMenuButtons.forEach(btn => btn.classList.remove("active"));
+              button.classList.add("active");
+
+              const filteredMenu =
+                  filter === "all"
+                      ? menus
+                      : menus.filter(menu => menu.category === filter);
+
+              renderMenuList(filteredMenu);
+          });
+      });
+
+      menuListElement.addEventListener("click", function (e) {
+        const card = e.target.closest(".menu-list-card");
+        if (!card) return;
+
+        const id = card.dataset.id;
+        const name = card.dataset.name;
+        const price = parseInt(card.dataset.price);
+        const image = card.dataset.image;
+
+        const activeOrders = orders[currentOrderType];
+
+        if (activeOrders[id]) {
+            activeOrders[id].qty += 1;
+        } else {
+            activeOrders[id] = {
+                id,
+                name,
+                price,
+                image,
+                qty: 1,
+                note: ""
+            };
+        }
+
+        renderOrderList();
+    });
